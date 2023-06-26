6f620f13d3b148e0dd8cabf2cc1da40c2804de33
Ramesh Pavan Pothamsetty <rameshpavan.p@outlook.com>
2023-06-26 15:30:22 -0400

    ...

---
+++ employee-system-backend/src/main/java/com/employesystem/employeesystembackend/model/Employee.java
@@ -0,0 +1,15 @@
+package com.employesystem.employeesystembackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+}
